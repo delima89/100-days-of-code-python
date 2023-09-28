7c376b17f10effd0764d86ba267f4b66949d6102
João Vitor <delimamoraisj@gmail.com>
2023-09-27 23:16:07 -0300

    oulha eu denovo

--- day 08/controle 8.4.py
+++ day 08/controle 8.4.py
@@ -1,33 +1,40 @@
+def encrypt(text,shift,diretion):
+    yes=0
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    while not yes =="no":
+        direction = input("digite 'encode' para codificado, degite 'decode' to decodificado:\n")
+        text = input("Type your message:\n").lower()
+        shift = int(input("digite um numero entre 1 e 9:\n"))
+        shift = shift% 26
+        codigo=""
+        if direction == "encode":
+            for char in text:
+                if char in alphabet:
+                    posição=alphabet.index(char)
+                    nova_posição=posição+shift
+                    nova_letra=alphabet[nova_posição]
+                    codigo+=nova_letra
+                else:
+                    codigo+=char
+            print(f"aqui esta sua mesangem codificada :{codigo} !")
+        elif direction == "decode":
+            for char in alphabet:
+                if char in alphabet:
+                    posição=alphabet.index(char)
+                    nova_posição=posição-shift
+                    nova_letra=alphabet[nova_posição]
+                    codigo+=nova_letra
+                else:
+                    codigo+=char
+        else:
+            print("erro")
+            print(f"aqui esta sua mensagem decodificado :{codigo} !")
+        yes=input("digite yes para fazer denovo ou no para encerra\n")
+direction = text = shift = codigo=""
+print("obrigado")
+encrypt(text,shift,direction)
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def encrypt(text,shift):
-    codigo=""
-    for letter in text:
-        posição=alphabet.index(letter)
-        nova_posição=posição+shift
-        nova_letra=alphabet[nova_posição]
-        codigo+=nova_letra
-    print(f"aqui esta sua mesangem codificada :{codigo} !")
-
-def decode(text,shift):
-    codigo=""
-    for letter in text:
-        posição=alphabet.index(letter)
-        nova_posição=posição-shift
-        nova_letra=alphabet[nova_posição]
-        codigo+=nova_letra
-    print(f"aqui esta sua mensagem decodificado :{codigo} !")
-yes=""
-while not yes =="no":
-    direction = input("digite 'encode' para codificado, degite 'decode' to decodificado:\n")
-    text = input("Type your message:\n").lower()
-    shift = int(input("digite um numero entre 1 e 9:\n"))
-    if str(direction) == "encode":
-        encrypt(text,shift)
-    elif direction == "decode":
-        decode(text,shift)
-    yes=input("digite yes para fazer denovo ou no para encerra\n")
 
 
 
